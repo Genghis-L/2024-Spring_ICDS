322b30a0417b7519f75601833462a4007237684b
Genghis Luo <125643679+Genghis-L@users.noreply.github.com>
2024-01-26 15:19:03 +0800

    Update of bonus

--- HW1/max_profit.py
+++ HW1/max_profit.py
@@ -9,12 +9,24 @@ Created on Tue Jan 23 14:08:56 2024
 
 def max_profit(prices):
     # input your code here
+    # Using 1 loop
+    p = []
+    for i in range(0, len(prices)-1):
+        p.append(max(prices[i+1:])-prices[i])
+
+    return max(p)
+
+
+''' 
+def max_profit(prices):
+    # Using nested loops
     p = []
     for i in range(0, len(prices)):
         for j in range(i+1, len(prices)):
             p.append(prices[j]-prices[i])
 
     return max(p)
+'''
 
 
 if __name__ == "__main__":
--- recitation_1.py
+++ recitation_1.py
@@ -6,46 +6,6 @@ Created on Thu Jan 25 10:54:12 2024
 @author: kehanluo
 """
 
-# Q1.2
-
-
-def get_most_frequent_charcter(s):
-    d = {}
-    for c in s:
-        d[c] = s.count(c)
-    freq = max(d.values())
-    for k in d:
-        if d[k] == freq:
-            print(k)
-            break
-
-
-get_most_frequent_charcter("taobao")
-
-
-# Q2.1
-
-def show_and_remove_larger_numbers(lst, n):
-    for i in lst:
-        if i > n:
-            print(i, end='')
-
-    print()
-
-    for i in lst.copy():
-        if i > n:
-            lst.remove(i)
-    print(lst)
-
-
-lst = [2, 3, 5, 1, 2, 6, 3, 4, 4]
-show_and_remove_larger_numbers(lst, 3)
-
-
-# Q3.1
-
-# def get_frequency(s):
-
 
 def add_up():
     lst = []
@@ -64,3 +24,87 @@ def add_up(n):
 
 
 print(add_up(100))
+
+
+def get_most_frequent_charcter(s):
+    ch = s[0]
+    count = 0
+    for c in s:
+        if s.count(c) > count:
+            ch = c
+            count = s.count(c)
+    print(ch)
+
+
+get_most_frequent_charcter("aaabbbc,cc")
+
+
+def show_and_remove_larger_numbers(lst, n):
+    for i in lst:
+        if i > n:
+            print(i, end='')
+
+    for i in lst.copy():
+        if i > n:
+            lst.remove(i)
+    print(lst)
+
+
+lst = [2, 3, 5, 1, 2, 6, 3, 4, 4]
+show_and_remove_larger_numbers(lst, 3)
+
+
+def get_frequency(s):
+    d = dict()
+    for c in s:
+        if c.isalnum():
+            d[c] = s.count(c)
+    print(d)
+    return d
+
+
+get_frequency("NYU SHANGHAI!!!!!!")
+
+
+x = 100
+
+
+def func_1():
+    print(x)
+
+
+def func_2():
+    x = 200
+
+    def func(x):
+        print(x)
+
+
+def change(n):
+    # n = ['Darth', 'Vader']
+    n.clear()
+    n.extend(['Darth', 'Vader'])
+
+
+name = ['Skywalker', 'Anakin']
+change(name)
+print(name)
+
+
+def add_up1(n):
+    lst = []
+    s = 0
+    for i in range(1, n+1):
+        s += i
+        lst.append(i)
+    print(sum(lst))
+    print(s)
+
+
+def add_up(n):
+    if n == 0:
+        return 0
+    return add_up(n-1) + n
+
+
+add_up(100)
