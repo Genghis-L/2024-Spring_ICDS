33b8276bb6f25490c94eb444c638cbf23c092157
Genghis Luo <125643679+Genghis-L@users.noreply.github.com>
2024-02-23 21:30:46 +0800

    HW4

--- HW4/bucket_sort.py
+++ HW4/bucket_sort.py
@@ -1,17 +1,27 @@
 import random
 random.seed(0)
 
+
 def bucket_sort(mylist):
     # initialize the buckets
     mydict = {}
-    
-    # place the values to be sorted in the buckets
 
-    # sort each bucket 
-    
+    # place the values to be sorted in the buckets
+    for num in mylist:
+        mydict[num // 10] = mydict.get(num // 10, []) + [num]
+        pass
+
+    # sort each bucket
+    for bucket in mydict.values():
+        bucket.sort()
+
     result = []
     # concatenate your bucket to the result
+    for key in sorted(mydict.keys()):
+        result.extend(mydict[key])
+
     return result
+
 
 def main():
     """ this is not exactly relevant, but the following 4 lines of
@@ -20,10 +30,11 @@ def main():
     """
     list_a = []
     for i in range(100):
-        list_a.append(random.randint(0,99))
+        list_a.append(random.randint(0, 99))
     print(list_a)
 
     list_a = bucket_sort(list_a)
-    print("SORTED:", list_a)    
+    print("SORTED:", list_a)
+
 
 main()
